514e877fb478ff77722296bcb57456d3b63f9cf7
y-aslani <77873489+y-aslani@users.noreply.github.com>
2021-01-23 12:21:18 +0330

    Create test.vue

---
+++ components/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{componentName}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'testComponent',
+        data() {
+            return {
+                componentName: 'hello world'
+            }
+        }
+    }
+</script>
