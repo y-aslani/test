9d16b0c4abd31608cfeff147453d0e256292fad4
yeganeh <y.aslani91@gmail.com>
2021-01-25 10:57:19 +0330

    third minor

--- components/test.vue
+++ components/test.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         test test v-1.1.2 {{componentName}}
-        
+        <div>
+            this is a test
+        </div>
+        <div>
+            this is a test
+        </div>
     </div>
 </template>
 
