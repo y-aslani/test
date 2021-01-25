72e69eedb9c960763ff52c67c23257a7cfa13054
yeganeh <y.aslani91@gmail.com>
2021-01-25 17:08:17 +0330

    new comment api added

---
+++ components/test2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        testttttt
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'testComponent2',
+        data() {
+            return {
+                componentName: 'hello world'
+            }
+        }
+    }
+</script>
